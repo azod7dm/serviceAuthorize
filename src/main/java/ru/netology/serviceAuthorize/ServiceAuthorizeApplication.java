59356ffe3a1bea02a8ff2802831c138e11222c19
Yaroslav Lagunin <yaric346@gmail.com>
2025-09-11 09:00:36 +0300

    add nginx.conf

--- src/main/java/ru/netology/serviceAuthorize/ServiceAuthorizeApplication.java
+++ src/main/java/ru/netology/serviceAuthorize/ServiceAuthorizeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServiceAuthorizeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceAuthorizeApplication.class, args);
 	}
